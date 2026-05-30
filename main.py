dfe96b7243d74e815f69ff8513439b408aa9e13d
Aryan <aryanbrite@gmail.com>
2026-05-30 13:11:36 +0530

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pdfplumber 
 st.set_page_config(
     page_icon="📚",
     page_title="FlashCard",
@@ -9,10 +9,15 @@ st.set_page_config(
 st.header("AI ANki Maker")
 cl1, cl2, cl3 = st.columns([2,3,2])
 with cl2:
-    st.file_uploader("Upload PDF",type=["pdf"])
+    b=st.file_uploader("Upload PDF",type=["pdf"])
     a=st.button("Submit")
 
-if a:
+if a and b:
     st.balloons()
+elif a:
+    with cl2:
+        st.warning("Please upload file first")
+
+
 
 
