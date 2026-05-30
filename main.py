a3a7faeecf925be3c6ae99dba3fce451c1c03539
Aryan <aryanbrite@gmail.com>
2026-05-30 12:58:55 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_icon="📚",
+    page_title="FlashCard",
+    layout="wide"
+)
+
+st.header("AI ANki Maker")
+cl1, cl2, cl3 = st.columns([2,3,2])
+with cl2:
+    st.file_uploader("Upload PDF",type=["pdf"])
+    a=st.button("Submit")
+
+if a:
+    st.balloons()
+
+
