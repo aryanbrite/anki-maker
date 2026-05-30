07dc9a63393b378b78c511ff0410de29f85c7f29
Aryan <aryanbrite@gmail.com>
2026-05-30 13:55:36 +0530

    Refactor PDF processing to extract text from all pages instead of just the first page

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pdfplumber
+from google import genai
 
 st.set_page_config(
     page_icon="📚",
@@ -16,8 +17,12 @@ with cl2:
 if a and b:
     st.balloons()
     with pdfplumber.open(b) as pdf:
-        es=pdf.pages[0].extract_table()
-    st.write(es)
+        txt=""
+        for i in pdf.pages:
+            a=i.extract_text()
+            if a:
+                txt=txt+a
+        st.write(txt)
 elif a:
     with cl2:
         st.warning("Please upload file first")
