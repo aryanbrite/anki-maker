9bbab5ce677da6bcb50e3cb5056cbb0498738fe4
Aryan <aryanbrite@gmail.com>
2026-05-30 14:42:09 +0530

    Add Google GenAI client and enhance PDF text processing for flashcard generation

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ st.set_page_config(
     page_title="FlashCard",
     layout="wide"
 )
+client=genai.Client(api_key="")
 
 st.header("AI ANki Maker")
 cl1, cl2, cl3 = st.columns([2,3,2])
@@ -22,7 +23,30 @@ if a and b:
             a=i.extract_text()
             if a:
                 txt=txt+a
-        st.write(txt)
+        prompt = f""" you are a flashcard generator. convert this text into question answer flashcard.
+        rules 
+        - keep answer short and concise. 
+        - you are suposed to write based on the text i am providing.
+        - no JSON
+        - no extra text
+
+        Return flashcards in this format:
+
+        Q: ...
+        A: ...
+
+        Q: ...
+        A: ...
+
+        text:
+        {txt}
+        """
+
+        responce= client.models.generate_content(
+            model="gemini-3.5-flash",
+            contents=prompt
+        )
+        s=responce.split("\n\n")
 elif a:
     with cl2:
         st.warning("Please upload file first")
