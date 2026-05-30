c9b2ee2030d9374122cf4a40bbcf6f741f272951
Aryan <aryanbrite@gmail.com>
2026-05-30 15:36:11 +0530

    Add Google GenAI API key and improve flashcard generation logic

--- main.py
+++ main.py
@@ -10,45 +10,47 @@ st.set_page_config(
 )
 client=genai.Client(api_key="")
 
-st.header("AI ANki Maker")
+st.markdown("# AI ANki Maker")
 cl1, cl2, cl3 = st.columns([2,3,2])
 with cl2:
     b=st.file_uploader("Upload PDF",type=["pdf"])
     a=st.button("Submit")
 
 if a and b:
+    with cl2:
+        with st.spinner("Reading PDF"):
+            with pdfplumber.open(b) as pdf:
+                txt=""
+                for i in pdf.pages:
+                    a=i.extract_text()
+                    if a:
+                        txt=txt+a
+                prompt = f""" you are a flashcard generator. convert this text into question answer flashcard.
+                rules 
+                - keep answer short and concise. 
+                - you are suposed to write based on the text i am providing.
+                - no JSON
+                - no extra text
 
-    with pdfplumber.open(b) as pdf:
-        txt=""
-        for i in pdf.pages:
-            a=i.extract_text()
-            if a:
-                txt=txt+a
-        prompt = f""" you are a flashcard generator. convert this text into question answer flashcard.
-        rules 
-        - keep answer short and concise. 
-        - you are suposed to write based on the text i am providing.
-        - no JSON
-        - no extra text
+                Return flashcards in this format:
 
-        Return flashcards in this format:
+                Q: ...
+                A: ...
 
-        Q: ...
-        A: ...
+                Q: ...
+                A: ...
 
-        Q: ...
-        A: ...
-
-        text:
-        {txt}
-        """
-
-        responce= client.models.generate_content(
-            model="gemini-3.5-flash",
-            contents=prompt
-        )
-        result = responce.text
-        blocks = result.strip().split("\n\n")
+                text:
+                {txt}
+                """
+    with cl2:
+        with st.spinner("Generating Flashcard"):
+            responce= client.models.generate_content(
+                model="gemini-3.5-flash",
+                contents=prompt
+            )
+            result = responce.text
+            blocks = result.strip().split("\n\n")
 
     flashcards = []
 
@@ -63,7 +65,6 @@ if a and b:
                 flashcards.append({"q": q, "a": a})
     st.markdown("# Preview")
     st.divider()
-    st.divider()
     for card in flashcards:
         st.markdown(f"""
                     ### Question
@@ -72,7 +73,16 @@ if a and b:
                     ### Answer
                     {card["a"]}""")
         st.divider()
-    df = pd.DataFrame
+    df = pd.DataFrame(flashcards)
+    csv = df.to_csv(index=False)
+    st.download_button(
+        label = "Download Anki File",
+        file_name = "anki-maker.csv",
+        mime = "text/csv",
+        data=csv,
+    )
+
+
     st.balloons()
 elif a:
     with cl2:
