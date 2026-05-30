ffcc9131650bda083d560869bd20e38af1416744
Aryan <aryanbrite@gmail.com>
2026-05-30 19:47:55 +0530

    Rename example.secrets.toml to secrets.toml and update API key retrieval in main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.set_page_config(
 )
 st.logo("https://cdn.hackclub.com/019e7928-ee48-79b3-8073-8098df0d8b90/Anki%20AI%20(1).png", size="large")
 
-client=genai.Client(api_key="")
+client=genai.Client(api_key=st.secrets["gemini_api"])
 
 cl1, cl2, cl3 = st.columns([2,3,2])
 with cl2:
